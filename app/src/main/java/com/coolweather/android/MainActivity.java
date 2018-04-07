2d8f3b40d2875d3ce0c3c31215fe0988c03954b0
Daxin <ehanyitong@126.com>
2018-04-07 12:27:32 +0800

    First commit

---
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -0,0 +1,14 @@
+package com.coolweather.android;
+//版本库的主页地址：https://github.com/lidaxin831/coolweather.git
+// 自己申请的本项目的GitHub地址：https://github.com/lidaxin831/coolweather
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
